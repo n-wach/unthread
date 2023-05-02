34159c22d8738d5e9df846a5b0cef889a14e2abc
n-wach <nmwachholz@gmail.com>
2023-05-02 19:41:10 +0000

    Explain END_EXIT code

--- src/include/unthread.h
+++ src/include/unthread.h
@@ -20,9 +20,10 @@ struct entropy_configuration {
       enum end_behavior {
         SCHEDULE_END_LOOP, // loop schedule data
         SCHEDULE_END_ZEROS, // continue with zeros
-        SCHEDULE_END_EXIT, // exit with code 0
+        SCHEDULE_END_EXIT, // exit with EXIT_ENTROPY + UNTHREAD_RET_OFFSET
         SCHEDULE_END_TRAP, // __builtin_trap()
-        // SCHEDULE_END_LONGJMP, // longjmp
+        // SCHEDULE_END_LONGJMP, // TODO: longjmp
+        // SCHEDULE_END_CALLBACK, // TODO: callback
       } end_behavior;
     } schedule;
   };
