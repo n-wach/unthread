fe22d10c0045a68877048d8c8383abfa065f2ee6
n-wach <nmwachholz@gmail.com>
2023-05-05 19:00:01 +0000

    Add interleave with checkpoints

--- targets/interleave_function_checkpoints.c
+++ targets/interleave_function_checkpoints.c
@@ -8,37 +8,57 @@ void* incr(int* val) {
   }
 }
 
+void ckpt0() {};
+void ckpt1() {};
+void ckpt2() {};
+void ckpt3() {};
+void ckpt4() {};
+void ckpt5() {};
+void ckpt6() {};
+void ckpt7() {};
+void ckpt8() {};
+void ckpt9() {};
+void ckpt10() {};
+
 // This is the kind of interleaving that a simple seed won't explore well...
 // We hope to show that a coverage-guided fuzzer, with fine-grained control
 // over the schedule, can reach the trap (a bug!)...
 void* buggy(int *val) {
-  unthread_yield();
+  ckpt1();
   if(*val != 0) return 0;
   unthread_yield();
 
+  ckpt2();
   if(*val != 2) return 0;
   unthread_yield();
 
+  ckpt3();
   if(*val != 5) return 0;
   unthread_yield();
 
+  ckpt4();
   if(*val != 7) return 0;
   unthread_yield();
 
+  ckpt5();
   if(*val != 12) return 0;
   unthread_yield();
 
+  ckpt6();
   if(*val != 15) return 0;
   unthread_yield();
 
+  ckpt7();
   if(*val != 15) return 0;
   unthread_yield();
   
+  ckpt8();
   if(*val == 17) {
     // a "bug"!
     __builtin_trap();
   }
 
+  ckpt0();
   return 0;
 }
 
