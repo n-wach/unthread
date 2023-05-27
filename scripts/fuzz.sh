44d1040cc1e081b5b4c0fded7d36fef8b6299dd5
n-wach <nmwachholz@gmail.com>
2023-05-27 06:58:01 +0000

    Simplify build process. Add scripts for testing with centipede.

--- scripts/fuzz.sh
+++ scripts/fuzz.sh
@@ -1,24 +1,12 @@
-set -e
 
-rm -r bin
-mkdir -p bin
+CENTIPEDE_BIN=$1
+TARGET_BIN=$2
 
-TARGET=interleave_fuzz_seed
-
-bazel build --config=centipede //targets:${TARGET}
-
-# We need to copy the executable because starlark doesn't allow
-# providing an executable not created by the rule
-cp bazel-bin/targets/${TARGET} bin/
-
-bazel build -c opt @com_google_fuzztest//centipede:centipede
-cp bazel-bin/external/com_google_fuzztest/centipede/centipede bin/
-
-cd bin/
-./centipede --binary=./${TARGET} --dry_run
+${CENTIPEDE_BIN} --binary="${TARGET_BIN}" --dry_run
 
 echo "==================================="
 echo "DRY RUN COMPLETE! FUZZING FOR REAL!"
 echo "==================================="
 
-time ./centipede --binary=./${TARGET} --exit_on_crash=true
+time ${CENTIPEDE_BIN} --binary="${TARGET_BIN}" --exit_on_crash=true
+
