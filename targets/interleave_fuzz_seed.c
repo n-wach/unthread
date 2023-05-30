962561c59e81aff0cb00e943fcf5f28accb4df03
n-wach <nmwachholz@gmail.com>
2023-05-30 23:52:21 +0000

    Refactor seed harness

--- targets/interleave_fuzz_seed.c
+++ targets/interleave_fuzz_seed.c
@@ -1,15 +1,17 @@
 #include "src/include/pthread.h"
 #include <stdio.h>
+#include <string.h>
 
 extern void interleave();
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   // Set up thread schedule.
-  if(size != sizeof(uint32_t[4])) {
+  uint32_t state[4] = {0, 0, 0, 0};
+
+  if(size > sizeof(state)) {
     return 0;
   }
-
-  uint32_t *state = ((uint32_t*) data);
+  memcpy((void *) state, data, size);
 
   unthread_configure((struct entropy_configuration){
     .entropy_source = ENTROPY_PRNG_SEED,
